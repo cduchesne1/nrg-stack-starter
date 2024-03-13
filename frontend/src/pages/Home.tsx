3a738fd18cb60d7fdd05f60044533c7bcf74dddf
Christophe Duchesne <cduchesne.1@gmail.com>
2024-03-12 22:05:25 -0400

    feat: connect frontend and backend with GraphQL

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import graphqlClient from '../../graphql/client';
+import { gql } from "@apollo/client";
+
 function Home() {
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const response = await graphqlClient.query({
+        query: gql`
+          query GetFirstUserUsername {
+            user(id: 1) {
+              username
+            }
+          }
+        `
+      });
+
+      setUsername(response.data.user.username);
+    };
+    fetchProfile();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Hi {username}!</h1>
       <p>Welcome to the home page!</p>
     </div>
   );
